1a4438cc3194608c40b7a96a0f695e7d95ed46de
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2023-12-04 20:57:53 +0900

    day1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
+use axum::{extract::Path, http::StatusCode, response::IntoResponse, routing::get, Router};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
@@ -8,11 +8,20 @@ async fn error() -> impl IntoResponse {
     (StatusCode::INTERNAL_SERVER_ERROR, "Internal Server Error")
 }
 
+async fn day1(Path(nums): Path<String>) -> impl IntoResponse {
+    let val = nums
+        .split('/')
+        .map(|num| num.parse::<i64>().unwrap())
+        .fold(0, |a, b| a ^ b)
+        .pow(3);
+    format!("{val}")
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
+        .route("/1/*nums", get(day1))
         .route("/-1/error", get(error))
         .route("/", get(hello_world));
-
     Ok(router.into())
 }
